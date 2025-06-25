2b4800800c1fcba2ca4e405e05ef3e32317c6f62
jssierrapi <jssierrapi@unadvirtual.edu.co>
2025-06-24 19:57:28 -0500

    fix: correct import path for populateDatabase script

--- api/src/routes/admin.js
+++ api/src/routes/admin.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { populateDatabase } = require('../scripts/populateDatabase');
+const { populateDatabase } = require('../../scripts/populateDatabase');
 const authenticate = require('../middleware/auth');
 const allowRoles = require('../middleware/roles');
 
